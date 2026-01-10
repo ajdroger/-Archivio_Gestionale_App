7b604710dfad44d4794da54cd1980629b2c46e50
ajdroger <ajmeer03@gmail.com>
2026-01-10 20:52:30 +0100

    feat(testing): k6 load test, playwright e2e, zap pipeline

---
+++ config/cloudflare.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Cloudflare WAF Configuration
+ * 
+ * TODO: Decommentare e configurare i valori reali quando il servizio sarà attivo.
+ */
+
+return [
+    'enabled' => filter_var($_ENV['CLOUDFLARE_ENABLED'] ?? false, FILTER_VALIDATE_BOOLEAN),
+
+    'api_token' => $_ENV['CLOUDFLARE_API_TOKEN'] ?? '',
+    'zone_id' => $_ENV['CLOUDFLARE_ZONE_ID'] ?? '',
+
+    /*
+    'waf_rules' => [
+        [
+            'action' => 'block',
+            'description' => 'Block SQL Injection attempts',
+            'expression' => '(http.request.uri.path contains "union" and http.request.uri.path contains "select")'
+        ],
+        [
+            'action' => 'challenge', // JS Challenge
+            'description' => 'Challenge suspicious User Agents',
+            'expression' => '(http.user_agent contains "curl" or http.user_agent contains "wget" or http.user_agent contains "python")'
+        ]
+    ],
+
+    'ip_whitelist' => [
+        '127.0.0.1', // Localhost
+        // '192.168.1.100', // Ufficio Amministrazione
+    ]
+    */
+];
