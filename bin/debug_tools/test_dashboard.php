227f4321e3029e20e65f3f5958e7d0585826445a
ajdroger <ajmeer03@gmail.com>
2026-01-15 07:56:30 +0100

    Polish dashboard UI: remove debug labels and error reporting

--- bin/debug_tools/test_dashboard.php
+++ bin/debug_tools/test_dashboard.php
@@ -11,10 +11,6 @@ date_default_timezone_set('Europe/Rome');
 
 // Secure Session Start (Matches Middleware)
 if (session_status() === PHP_SESSION_NONE) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
     ini_set('session.cookie_httponly', 1);
     ini_set('session.cookie_samesite', 'Lax');
     ini_set('session.cookie_path', '/');
